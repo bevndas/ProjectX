b355a7c215b73b2beb73d7c86095d6c45ba888f8
bipinshrestha <bipinshrestha0018@gmail,com>
2017-09-13 22:18:39 +0545

    First Commit with Register Form

---
+++ assets/js/radio.js
@@ -0,0 +1,13 @@
+// Included fastclick.js to make the inputs quicker to respond to taps on mobile
+
+
+
+
+ /** JS for radio button **/
+
+ 
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+        FastClick.attach(document.body);
+    }, false);
+}
